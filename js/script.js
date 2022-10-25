881cc466177093ecd88ca8cbd959970386feb680
ThiagoDiniz-creator <thiago.diniz.20.03.2002@gmail.com>
2022-10-25 08:03:54 -0300

    Fixed an issue with the sticky navbar opening the mobile navigation.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,61 @@
-function updateYear(){
-    const copyright = document.querySelector("p.copyright");
-    if(copyright){
-        const year = new Date().getFullYear();
-        copyright.innerHTML = "Copyright © " + year + " by Omnifood Inc.";
+function updateYear() {
+  const copyright = document.querySelector("p.copyright");
+  if (copyright) {
+    const year = new Date().getFullYear();
+    copyright.innerHTML = "Copyright © " + year + " by Omnifood Inc.";
+  }
+}
+
+function handleMobileNavigation() {
+  const btnNavEl = document.querySelector(".btn-mobile-nav");
+  const headerEl = document.querySelector(".header");
+  const mainNavButtonsEl = document.querySelectorAll("a.main-nav-link");
+
+  // Adding the listener to the click event on the mobileNav button.
+    btnNavEl.addEventListener("click", function () {
+      headerEl.classList.toggle("nav-open");
+  });
+
+  // Adding listeners to the buttons on the mobile nav.
+  mainNavButtonsEl.forEach((el) => {
+     el.addEventListener('click', function (){
+         if(headerEl.classList.contains("nav-open"))
+             headerEl.classList.toggle("nav-open");
+     });
+  });
+}
+
+function stickyNavbar() {
+  const body = document.querySelector("body");
+  const navbarEl = document.querySelector(".main-nav");
+  const heroSectionEl = document.querySelector("section.section-hero");
+  const headerEl = document.querySelector("#header");
+
+  const obs = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+
+      if (headerEl.classList.contains("nav-open")) return;
+
+      if (!ent.isIntersecting) body.classList.add("sticky");
+      else body.classList.remove("sticky");
+    },
+    {
+      // In the viewport
+      root: null,
+      // The event fires when 0% of the target enters the viewport
+      threshold: 0,
+      // Changing the root margin
+      rootMargin: "-80px",
     }
+  );
+  obs.observe(heroSectionEl);
 }
 
-function handleMobileNavigation(){
-    const btnNavEl = document.querySelector(".btn-mobile-nav");
-    const headerEl = document.querySelector(".header");
-
-    btnNavEl.addEventListener("click", function (){
-       headerEl.classList.toggle("nav-open");
-    });
-}
-
-function stickyNavbar(){
-    const navbarEl = document.querySelector("#header");
-    const heroSectionEl = document.querySelector("section.section-hero");
-    const obs = new IntersectionObserver(function (entries){
-        const ent = entries[0];
-        if(!ent.isIntersecting){
-            navbarEl.classList.add("sticky");
-            heroSectionEl.classList.add("fix-height");
-        }
-        else{
-            navbarEl.classList.remove("sticky");
-            heroSectionEl.classList.remove("fix-height");
-        }
-    }, {
-        // In the viewport
-        root: null,
-        // The event fires when 0% of the target enters the viewport
-        threshold: 0,
-        // Changing the root margin
-        rootMargin: "-80px",
-    });
-    obs.observe(heroSectionEl);
-}
-
-function main(){
-    updateYear();
-    handleMobileNavigation();
-    stickyNavbar();
+function main() {
+  updateYear();
+  handleMobileNavigation();
+  stickyNavbar();
 }
 
 main();
